ad6a4c55b0b15a9005deaa2fd820d214ac159f3b
SamMartin92 <samm92@live.ie>
2024-04-08 21:48:21 +0000

    grid button creates grid

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let grid = document.getElementById('grid');
-let gridButton =document.getElementById('grid-btn');
+const grid = document.getElementById('grid');
+const container =document.getElementById('container');
+const gridButton = document.getElementById('grid-btn');
+const rowsInput = document.getElementById('rows');
+const colsInput = document.getElementById('columns');
 
 
 
 
-function setGridHTML(j){
-    let tilesHTML ='';
-    for (let i = 0; i < j; i++) {
-        tilesHTML += '<div class="tile hover"></div>';
-    }
-    grid.innerHTML = tilesHTML;
-    grid.classList.add('container')
+function setGridHTML(rows, cols) {
+    container.innerHTML = '';
+    container.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`);
+    container.style.setProperty('grid-template-columns', `repeat(${cols}, 1fr)`);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+      };  
 }
 
-gridButton.addEventListener('click',function(){
-    setGridHTML(9)
+gridButton.addEventListener('click', function () {
+    const rows = rowsInput.value;
+    const cols = colsInput.value;
+    setGridHTML(rows, cols);
 });
 
