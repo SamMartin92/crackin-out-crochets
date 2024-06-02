4f172bbb6512f4652b28e3f69af37b6bace7af7b
SamMartin92 <samm92@live.ie>
2024-06-02 20:36:31 +0000

    set grid distinguishes outer cells

--- script.js
+++ script.js
@@ -29,13 +29,18 @@ function setGridHTML(rows, cols) {
         for (let c = 0; c < cols; c++) {
             let cell = document.createElement("div");
             cell.setAttribute('id', `${r}.${c}`);
-            container.appendChild(cell).className = "grid-item";
-            cell.addEventListener('click', () => fillCell(cell));
-            cell.addEventListener('mouseenter', () => {
-                if (mouseDown){
-                    fillCell(cell);
-                }
-            });
+
+            if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
+                container.appendChild(cell).className = "outer-cell"
+            } else {
+                container.appendChild(cell).className = "grid-item";
+                cell.addEventListener('click', () => fillCell(cell));
+                cell.addEventListener('mouseenter', () => {
+                    if (mouseDown) {
+                        fillCell(cell);
+                    }
+                });
+            }
         }
     };
 }
@@ -57,8 +62,8 @@ function fillGrid() {
 }
 
 gridButton.addEventListener('click', () => {
-    const rows = parseInt(rowsInput.value)+2;
-    const cols = parseInt(colsInput.value)+2;
+    const rows = parseInt(rowsInput.value) + 2;
+    const cols = parseInt(colsInput.value) + 2;
     setGridHTML(rows, cols);
     fillGrid();
 });
